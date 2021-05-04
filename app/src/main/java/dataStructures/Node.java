bb351b3c3296a4e3e26ad32ec148450b20be906d
Elizabeth Hayt <enhayt@gmail.com>
2021-05-04 12:44:30 -0700

    Copy over linkedList files and tests

---
+++ app/src/main/java/dataStructures/Node.java
@@ -0,0 +1,23 @@
+package dataStructures;
+
+public class Node {
+
+    private Node next;
+    private int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setNext(Node nextNode) {
+        this.next = nextNode;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+}
---
+++ app/src/test/java/dataStructures/NodeTest.java
@@ -0,0 +1,29 @@
+package dataStructures;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    @DisplayName("Can create a node with integer")
+    void nodeShouldCreatedWithInteger() {
+        Node node = new Node(10);
+        int expected = 10;
+        int actual = node.getData();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("New node's next node is null")
+    void nodeShouldCreatedWithNextNullPointer() {
+        Node node = new Node(10);
+        Node actual = node.getNext();
+        assertNull(actual);
+    }
+
+
+
+}
