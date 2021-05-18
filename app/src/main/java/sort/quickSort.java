1466df5a7c60496454c60a13914811dcf2b98fe4
Elizabeth Hayt <enhayt@gmail.com>
2021-05-18 13:31:58 -0700

    Implement recursive quicksort

--- app/src/main/java/sort/quickSort.java
+++ app/src/main/java/sort/quickSort.java
@@ -2,10 +2,37 @@ package sort;
 
 public class quickSort {
 
-    public static int[] quicksort(int[] input) {
-        if (input.length == 0 || input.length == 1) {
-            return input;
+    public static void quicksort(int[] input) {
+        quicksort(input, 0, input.length - 1);
+    }
+
+    public static void quicksort(int[] input, int leftP, int rightP) {
+        if (leftP >= rightP) {
+            return;
         }
-        return input;
+        int pivot = input[(leftP + rightP) / 2];
+        int index = partition(input, leftP, rightP, pivot);
+        quicksort(input, leftP, index - 1);
+        quicksort(input, index, rightP);
+    }
+
+    public static int partition(int[] input, int leftP, int rightP, int pivot) {
+        while (leftP <= rightP) {
+            while (input[leftP] < pivot) {
+                leftP++;
+            }
+            while (input[rightP] > pivot) {
+                rightP--;
+            }
+            if (leftP <= rightP) {
+                int tempR = input[rightP];
+                int tempL = input[leftP];
+                input[rightP] = tempL;
+                input[leftP] = tempR;
+                leftP++;
+                rightP--;
+            }
+        }
+        return leftP;
     }
 }
--- app/src/test/java/sort/quickSortTest.java
+++ app/src/test/java/sort/quickSortTest.java
@@ -1,12 +1,15 @@
 package sort;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class quickSortTest {
     private int[] allPositive = {3, 4, 1, 8, 2};
+
+    private int[] threeNums = {5, 7, 6};
 
     private int[] withNegatives = {3, -4, 1, -8, 2};
 
@@ -19,21 +22,42 @@ public class quickSortTest {
     @Test
     public void quickSort_canHandleInputArrayOfSize0() {
         int[] expected = {};
-        int[] actual = quickSort.quicksort(emptyArray);
-        assertArrayEquals(expected, actual);
+        quickSort.quicksort(emptyArray);
+        assertArrayEquals(expected, emptyArray);
     }
 
     @Test
     public void quickSort_canHandleInputArrayOfSize1() {
         int[] expected = {1};
-        int[] actual = quickSort.quicksort(sizeOne);
-        assertArrayEquals(expected, actual);
+        quickSort.quicksort(sizeOne);
+        assertArrayEquals(expected, sizeOne);
+    }
+
+    @Test
+    public void quickSort_sortsArrayOf3Numbers() {
+        int[] expected = {5, 6, 7};
+        quickSort.quicksort(threeNums);
+        assertArrayEquals(expected, threeNums);
     }
 
     @Test
     public void quickSort_sortsArrayOfPositiveNumbers() {
         int[] expected = {1, 2, 3, 4, 8};
-        int[] actual = quickSort.quicksort(allPositive);
-        assertArrayEquals(expected, actual);
+        quickSort.quicksort(allPositive);
+        assertArrayEquals(expected, allPositive);
+    }
+
+    @Test
+    public void quickSort_sortsArrayWithNegativeNumbers() {
+        int[] expected = { -8, -4, 1, 2, 3};
+        quickSort.quicksort(withNegatives);
+        assertArrayEquals(expected, withNegatives);
+    }
+
+    @Test
+    public void quickSort_sortsArrayWithDuplicateNumbers() {
+        int[] expected = { 1, 2, 2, 3, 4, 4, 8};
+        quickSort.quicksort(withDoubles);
+        assertArrayEquals(expected, withDoubles);
     }
 }
